dab64298c7a32fd3b2efaaa1f67ca8029042c9d5
Daniel Moreno <daniel10697@hotmail.com>
2022-01-20 21:17:31 +0100

    Restructure scaffolding project

---
+++ src/Pokemon/Type/infraestucture/PokeApiTypeRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Pokemon.Type.domain;
+
+namespace Pokemon.Type.infraestucture
+{
+    public class PokeApiRepository : ITypeRepository
+    {
+        public List<Type> PokeApiRepository()
+        {
+            
+        }
+    }
+}
