3c4406398cb1c92b006151d8567e23e37c6a3e96
rcamison <85695417+rcamison@users.noreply.github.com>
2022-02-03 21:05:19 +0100

    User Bounded Context Init

---
+++ apps/UserApi/Controllers/AddPokemonFavoriteController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace UserApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserCreateController : ControllerBase
+    {
+
+    }
+}
