7bc8e884e16460734773691ad170313ee1eaefb7
rcamison <85695417+rcamison@users.noreply.github.com>
2022-02-09 21:08:55 +0100

    Test project

---
+++ tests/UsersTest/application/CreateUserUseCaseTest.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace UsersTest.application
+{
+    public class CreateUserUseCaseTest
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ tests/UsersTest/domain/UserMother.cs
@@ -0,0 +1,8 @@
+﻿namespace UsersTest.domain{
+
+public class UserMother
+{
+    
+}
+
+}
