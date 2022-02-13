e90e22a21065996b406a5f5f3aa033e73bda0972
rcamison <85695417+rcamison@users.noreply.github.com>
2022-02-13 23:42:58 +0100

    ObjectMother for PokemonFavorite

---
+++ tests/UsersTest/domain/PokemonFavoriteMother.cs
@@ -0,0 +1,20 @@
+﻿using Moq;
+using Users.User.domain;
+
+namespace UsersTest.domain
+{
+
+    public class PokemonFavoriteMother 
+    {
+        public static PokemonFavorite Random()
+        {
+            return PokemonFavorite.Create(It.IsAny<PokemonId>()); 
+        }
+
+        public static PokemonFavorite Random(PokemonId pokemonId)
+        {
+            return PokemonFavorite.Create(pokemonId);
+        }
+    }
+
+}
