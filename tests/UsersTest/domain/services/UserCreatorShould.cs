0dd7cb2e76b4d1c8b44a7a6ff810c60302ea3ccf
rcamison <85695417+rcamison@users.noreply.github.com>
2022-02-13 23:43:35 +0100

    Refactor UseCreatorShould

--- tests/UsersTest/domain/services/UserCreatorShould.cs
+++ tests/UsersTest/domain/services/UserCreatorShould.cs
@@ -1,54 +1,44 @@
 ﻿using Moq;
-using System;
 using Users.User.domain;
-using Users.User.infraestructure;
 using Xunit;
 
 namespace UsersTest.domain.services
 {
     public class UserCreatorShould
     {
-       // private readonly IUserRepository _userRepository;
-        private UserCreator? _creator;
-
+        private UserCreator? _userCreator;
 
         [Fact]
-        public void Should_Create_A_User()
+        public void Create_A_User()
         {
             var userRepository = new Mock<IUserRepository>();
             userRepository.Setup(_ => _.Exists(It.IsAny<UserId>())).Returns(false);
-            _creator = new UserCreator(userRepository.Object);
+            _userCreator = new UserCreator(userRepository.Object);
 
             //Given
             var userId = UserIdMother.Random();
             var userName = UserNameMother.Random();
 
             //When
-            _creator.Execute(userId, userName);
+            _userCreator.Execute(userId, userName);
 
             //Then
-            var user = UserMother.Random(userId,userName);
-            
-            //funciona
             userRepository.Verify(v => v.Save(It.IsAny<User>())); 
-
-            //no funciona
-            userRepository.Verify(v => v.Save(user));
         }
 
         [Fact]
-        public void Should_Throw_An_Exception_When_User_Already_Exists()
+        public void Throw_An_Exception_When_User_Already_Exists()
         {
             var userRepository = new Mock<IUserRepository>();
             userRepository.Setup(_ => _.Exists(It.IsAny<UserId>())).Returns(true);
-            _creator = new UserCreator(userRepository.Object);
+            _userCreator = new UserCreator(userRepository.Object);
 
             //Given
             var userId = UserIdMother.Random();
             var userName = UserNameMother.Random();
 
             //When - Then
-            Assert.Throws<UserAlreadyExistsException>(() => _creator.Execute(userId, userName)); 
+            Assert.Throws<UserAlreadyExistsException>(() => _userCreator.Execute(userId, userName)); 
         }
     }
 }
