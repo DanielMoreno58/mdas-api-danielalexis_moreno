407c7f931f6379e211ec79a445ca2d910478c781
dcervantes <dcervantesr@gmail.com>
2022-02-17 21:12:47 +0100

    unit test GetTypesByPokemonNameUseCaseTest

--- tests/TypeTests/application/GetTypesByPokemonNameUseCaseTest.cs
+++ tests/TypeTests/application/GetTypesByPokemonNameUseCaseTest.cs
@@ -1,14 +1,26 @@
+using System.Collections.Generic;
+using Moq;
 using Pokemon.Type.Application;
+using Pokemon.Type.Domain;
+using TypeTest.Domain;
 using Xunit;
 
-namespace UsersTest.Application
+namespace TypeTest.Application
 {
     public class GetTypesByPokemonNameUseCaseTest
     {
         [Fact]
         public void Should_Return_A_Types_By_PokemonName()
         {
-            
+            var typeRepository = new Mock<ITypeRepository>();
+            var type = TypeMother.Random();
+            typeRepository.Setup(_ => _.FindByPokemonName(It.IsAny<PokemonName>())).Returns(new List<Type>() { type });
+            var findByPokemonName = new Mock<FindByPokemonName>(typeRepository.Object);
+            var getTypesByPokemonNameUseCase = new GetTypesByPokemonNameUseCase(findByPokemonName.Object);
+
+            var result = getTypesByPokemonNameUseCase.Execute(new GetTypesByPokemonNameQuery("pikachu"));
+
+            Assert.Equal(type.Name, result[0].Name);
         }
     }
 }
--- tests/UsersTest/domain/UserMother.cs
+++ tests/UsersTest/domain/UserMother.cs
@@ -1,5 +1,4 @@
-﻿
-using Moq;
+﻿using Moq;
 using Users.User.Domain;
 
 namespace UsersTest.Domain
@@ -17,5 +16,4 @@ namespace UsersTest.Domain
             return User.Create(userId, userName);
         }
     }
-
 }
