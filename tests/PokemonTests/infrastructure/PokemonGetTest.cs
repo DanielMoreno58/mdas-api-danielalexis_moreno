79effad6708522e1786edbf9e283d912a3cbe035
rcamison <85695417+rcamison@users.noreply.github.com>
2022-02-19 17:36:46 +0100

    PokemonGet acceptance test

---
+++ tests/PokemonTests/infrastructure/PokemonGetTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Xunit;
+using System.Text;
+
+
+namespace PokemonTests.Infrastructure
+{
+    public class PokemonGetTest
+    {
+
+        private static HttpClient client = new HttpClient();
+
+        [Fact, Trait("Type", "Acceptance")]
+        private void Should_Get_A_Pokemon_By_Its_Id()
+        {
+            HttpClient httpClient = new HttpClient();
+            string getUrl = "http://pokemon:80/api/v1/pokemon/1";
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            Guid id = Guid.NewGuid();
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, getUrl);
+            var response = httpClient.SendAsync(request).GetAwaiter().GetResult();
+            Assert.True(response.IsSuccessStatusCode);
+        }
+    }
+
+}
