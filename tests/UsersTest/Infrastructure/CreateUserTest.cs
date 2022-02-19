c9bb110f1e1ea2c2e8dd863502d91b5940b5c953
rcamison <85695417+rcamison@users.noreply.github.com>
2022-02-19 16:52:05 +0100

    Refactor CreateUserTest

--- tests/UsersTest/Infrastructure/CreateUserTest.cs
+++ tests/UsersTest/Infrastructure/CreateUserTest.cs
@@ -6,22 +6,22 @@ using System.Text;
 
 namespace UsersTest.infraestructure
 {
-    public class CreateUser
+    public class CreateUserTest
     {
 
         private static HttpClient client = new HttpClient();
-        
+
         [Fact]
         private void Should_Create_New_User()
         {
             HttpClient httpClient = new HttpClient();
-            string postUrl = "http://localhost:4080/api/v1/users";
+            string postUrl = "http://userfavorite:80/api/v1/users";
             client.DefaultRequestHeaders.Accept.Clear();
             client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
             Guid id = Guid.NewGuid();
             HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, postUrl);
-            request.Content = new StringContent("{\"name\":\"Jonh Doe\",\"id\":\""+id+"\"}",
-                                                Encoding.UTF8, 
+            request.Content = new StringContent("{\"name\":\"Jonh Doe\",\"id\":\"" + id + "\"}",
+                                                Encoding.UTF8,
                                                 "application/json");
             var response = httpClient.SendAsync(request).GetAwaiter().GetResult();
             Assert.True(response.IsSuccessStatusCode);
